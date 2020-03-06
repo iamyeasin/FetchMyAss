3bab3000ed7ab160b2d70aea9f5261eae3455016
yeasin Rahman <ryeashin@gmail.com>
2020-03-06 17:06:53 +0600

    fourth Change

--- background.js
+++ background.js
@@ -25,7 +25,7 @@ function sendMyAss(formData){
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-     console.log("fuckit");
+     // console.log("fuckit");
     }
   };
   xhttp.open("POST", url, true);
